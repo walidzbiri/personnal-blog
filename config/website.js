a04bcb944d4f83ff7a880409b956593bb712e206
walidzbiri <walid.zbiri@uit.ac.ma>
2020-05-25 14:33:36 +0000

    modified:   config/website.js

--- config/website.js
+++ config/website.js
@@ -2,7 +2,7 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/blog your pathPrefix should be "blog"
   siteTitle: 'Walid ZBIRI', // Navigation and Site Title
   siteTitleAlt: 'Walid ZBIRI personal blog ', // Alternative Site title for SEO
-  siteTitleShort: 'Personal blog', // short_name for manifest
+  siteTitleShort: 'WAlid ZBIRI blog', // short_name for manifest
   siteUrl: 'https://walidzbiri.me', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: 'images/logo.png', // Used for SEO and manifest, path to your image you placed in the 'static' folder
